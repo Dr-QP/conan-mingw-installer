34aa3cd5909fc3be70efc94b8e36391dbb4fd3d2
Anton Matosov <anton.matosov@gmail.com>
2017-07-17 20:55:16 -0700

    Implement generation of all possible options combinations

--- build.py
+++ build.py
@@ -13,6 +13,32 @@ class WindowsPackager(ConanMultiPackager):
 
 if __name__ == "__main__":
     builder = WindowsPackager(args="--build missing")
-    builder.add({})
+    possible_options = {"threads": ["posix", "win32"],
+                "exception": ["dwarf2", "sjlj", "seh"],
+                "arch": ["x86", "x86_64"],
+                "version": ["4.8", "4.9", "5.4", "6.2"]}
+
+    bad_options = [{
+        "arch": "x86",
+        "exception": "seh"
+    }, {
+        "arch": "x86_64",
+        "exception": "dwarf2"
+    }]
+
+    build_options = [{}]
+    for name, values in possible_options.iteritems():
+        new_build_options = []
+        for value in values:
+            for options in build_options:
+                new_options = copy.copy(options)
+                new_options[name] = value
+                new_build_options.append(new_options)
+        build_options = new_build_options
+
+    print("Options list:")
+    for p in build_options:
+        print p
+    # builder.add({})
 
     builder.run()
