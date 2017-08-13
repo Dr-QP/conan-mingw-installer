9003c9609bbfd11b8ed5d5d65a44d54006c193bd
Anton Matosov <anton.matosov@gmail.com>
2017-08-12 17:49:43 -0700

    Build all versions available, but upload only latest

--- build.py
+++ build.py
@@ -7,7 +7,9 @@ if __name__ == "__main__":
     base = {"os": "Windows", "compiler": "gcc", "compiler.version": "7.1",
             "compiler.exception": "seh", "compiler.libcxx": "libstdc++",
             "compiler.threads": "posix"}
-    for version in ["4.9", "5.4", "6.3", "7.1"]:
+    versionsToUpload = ["4.9", "5.4", "6.3", "7.1"]
+    versionsToBuildOnly = ["4.8", "5.3"]
+    for version in versionsToUpload + versionsToBuildOnly:
         tmp = copy.copy(base)
         tmp["compiler.version"] = version
         for th in ["posix", "win32"]:
@@ -16,6 +18,6 @@ if __name__ == "__main__":
             for ex in ["seh", "sjlj"]:
                 tmp3 = copy.copy(tmp2)
                 tmp3["compiler.exception"] = ex
-                builder.add(tmp3, {}, {}, {})
+                builder.add(tmp3, {}, {}, {}, upload=(version in versionsToUpload))
     
     builder.run()
